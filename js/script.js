0ff75502c374da7f9377169bba4ae9d142788de5
cristiandkw <cristian_dkw@hotmail.com>
2023-02-28 15:30:49 -0300

    fix errors

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 
 
-document.getElementById('boton').addEventListener('click', function () {
+document.getElementById('boton_color').addEventListener('click', function () {
     document.body.style.backgroundColor = "#FF0000";
 
 
 });
+document.getElementById('boton_default').addEventListener('click', function (){
+    document.body.style.backgroundColor ='#beb8ac';
+
+  });
